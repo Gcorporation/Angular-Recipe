2d828e0fc0a8e4bfd6e2ba662e3756abfadb4b43
Prabhjot Singh <prabhjotsingh394@gmail.com>
2022-09-26 23:03:24 +0530

    Multiple Actions

--- src/app/shopping-list/store/shopping-list.actions.ts
+++ src/app/shopping-list/store/shopping-list.actions.ts
@@ -2,6 +2,7 @@ import { Action } from "@ngrx/store";
 import { Ingredient } from "src/app/shared/ingredient.model";
 
 export const ADD_INGREDIENT = 'ADD_INGREDIENT';
+export const ADD_INGREDIENTS = 'ADD_INGREDIENTS';
 
 export class ADDIngredient implements Action {
     
@@ -9,3 +10,12 @@ export class ADDIngredient implements Action {
 
     constructor(public payload: Ingredient) {}
 }
+
+export class ADDIngredients implements Action {
+    
+    readonly type = ADD_INGREDIENTS;
+
+    constructor(public payload: Ingredient[]) {}
+}
+
+export type ShoppingListActions  = ADDIngredient | ADDIngredients;
